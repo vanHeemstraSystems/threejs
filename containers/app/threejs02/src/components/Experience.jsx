1591c2dc355ca4e35fb8f6a15dda23a2bbd95ee0
Willem van Heemstra <wvanheemstra@icloud.com>
2023-10-05 18:21:05 +0000

    Replace mesh with Office component

--- containers/app/threejs02/src/components/Experience.jsx
+++ containers/app/threejs02/src/components/Experience.jsx
@@ -1,13 +1,11 @@
 import { OrbitControls } from "@react-three/drei"
+import { Office } from "./Office"
 
 export const Experience = () => {
     return (
         <>
             <OrbitControls />
-            <mesh>
-                <boxGeometry />
-                <meshNormalMaterial />
-            </mesh>
+            <Office />
         </>
     )
 }
